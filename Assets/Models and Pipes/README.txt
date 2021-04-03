7234fe1f1bb34c9b1a3028c0da3e23e27f6c446a
Indika Wijesooriya <indikman@gmail.com>
2021-04-03 11:57:02 +0530

    Initial Commit

---
+++ Assets/Models and Pipes/README.txt
@@ -0,0 +1,7 @@
+Modular Pipes Pack:
+	Low poly
+	2048x2048 Textures
+	For PC & Console & VR
+	13 Prefabs
+	Demo Scene
+Enjoy!
---
+++ Assets/Prepared/Scripts/FlowManagerPrepared.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowManagerPrepared : MonoBehaviour
+{
+    // Get the list of gameobjects
+    public GameObject[] GuideSteps;
+    public Transform StepHolder;
+
+    private int currentStep = 0;
+
+    private void Start()
+    {
+        ResetFlow();
+    }
+
+    public void ChangeStep(bool isNext)
+    {
+        if (isNext)
+        {
+            if(currentStep >= GuideSteps.Length-1)
+            {
+                Debug.Log("Reached the end of the steps");
+                return;
+            }
+
+            currentStep++;
+
+        }
+        else
+        {
+            if(currentStep <= 0)
+            {
+                Debug.Log("No more steps before this");
+                return;
+            }
+
+            currentStep--;
+        }
+
+        HideAllSteps();
+        GuideSteps[currentStep].SetActive(true);
+
+    }
+
+    private void HideAllSteps()
+    {
+        foreach(Transform child in StepHolder)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    public void ResetFlow()
+    {
+        HideAllSteps();
+        GuideSteps[0].SetActive(true);
+        currentStep = 0;
+    }
+
+
+}
